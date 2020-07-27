433344d3599b06d78516d4905b757069b63d4038
suryasuryo00 <34580117+suryasuryo00@users.noreply.github.com>
2020-07-27 20:27:46 +0700

    Update Merza1.php

--- Merza1.php
+++ Merza1.php
@@ -29,7 +29,7 @@ echo "\n".color("white","?] Mau Redeem Voucher?: y/n ");
         echo color("nevy",".");
         sleep(10);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2107"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"STAYGOFOOD201105SCE"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","🔓▶Message: ".$message);
